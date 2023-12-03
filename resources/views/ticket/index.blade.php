734ce4d81160b1252ef07e8198c001096b591da6
hardycapel-grh <57265205+hardycapel-grh@users.noreply.github.com>
2023-12-03 22:20:15 +0000

    rev check

--- resources/views/ticket/index.blade.php
+++ resources/views/ticket/index.blade.php
@@ -1,3 +1,4 @@
+{{-- latest --}}
 <x-app-layout>
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
         <div class="flex justify-between w-full sm:max-w-xl">
